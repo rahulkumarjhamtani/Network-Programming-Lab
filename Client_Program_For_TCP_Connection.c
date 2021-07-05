2e5571a1ec1aa2bbae2e1c27029c08d6fbef6446
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-05 23:06:43 +0530

    lab

--- Client_Program_For_TCP_Connection.c
+++ Client_Program_For_TCP_Connection.c
@@ -15,95 +15,63 @@
 #include <netdb.h>
 
 void error(const char *msg)
-
 {
-
-perror(msg);
-
-exit(0);
-
+    perror(msg);
+    exit(0);
 }
 
 int main(int argc, char *argv[])
-
 {
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+    if (argc< 3)
+    {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
 
-int sockfd, portno, n;
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-struct sockaddr_in serv_addr;
+    if (sockfd< 0)
+        error("ERROR opening socket");
 
-struct hostent *server;
+    server = gethostbyname(argv[1]);
 
-char buffer[256];
+    if (server == NULL)
+    {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
 
-if (argc< 3)
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    serv_addr.sin_port = htons(portno);
 
-{
+    if(connect(sockfd, &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
 
-fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
 
-exit(0);
+    n = write(sockfd,buffer,strlen(buffer));
 
-}
+    if (n < 0)
+        error("ERROR writing to socket");
 
-portno = atoi(argv[2]);
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
 
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (n < 0)
+        error("ERROR reading from socket");
 
-if (sockfd< 0)
+    printf("%s\n",buffer);
+    close(sockfd);
 
-error("ERROR opening socket");
-
-server = gethostbyname(argv[1]);
-
-if (server == NULL)
-
-{
-
-fprintf(stderr,"ERROR, no such host\n");
-
-exit(0);
-
-}
-
-bzero((char *) &serv_addr, sizeof(serv_addr));
-
-serv_addr.sin_family = AF_INET;
-
-bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,
-
-server->h_length);
-
-serv_addr.sin_port = htons(portno);
-
-if(connect(sockfd, &serv_addr,sizeof(serv_addr)) < 0)
-
-error("ERROR connecting");
-
-printf("Please enter the message: ");
-
-bzero(buffer,256);
-
-fgets(buffer,255,stdin);
-
-n = write(sockfd,buffer,strlen(buffer));
-
-if (n < 0)
-
-error("ERROR writing to socket");
-
-bzero(buffer,256);
-
-n = read(sockfd,buffer,255);
-
-if (n < 0)
-
-error("ERROR reading from socket");
-
-printf("%s\n",buffer);
-
-close(sockfd);
-
-return 0;
+    return 0;
 
 }
