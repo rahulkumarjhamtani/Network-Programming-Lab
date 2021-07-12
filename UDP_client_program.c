e8b623162c1a2753f8a37e85970b953b8e482c01
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-07-12 13:29:34 +0530

    lab

--- UDP_client_program.c
+++ UDP_client_program.c
@@ -1,3 +1,5 @@
+// UDP Client
+
 #include <stdio.h>
 
 #include <sys/socket.h>
@@ -8,54 +10,54 @@
 
 int main(){
 
-int clientSocket, portNum, nBytes;
+    int clientSocket, portNum, nBytes;
 
-char buffer[1024];
+    char buffer[1024];
 
-struct sockaddr_in serverAddr;
+    struct sockaddr_in serverAddr;
 
-socklen_t addr_size;
+    socklen_t addr_size;
 
-/*Create UDP socket*/
+    /*Create UDP socket*/
 
-clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+    clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-/*Configure settings in address struct*/
+    /*Configure settings in address struct*/
 
-serverAddr.sin_family = AF_INET;
+    serverAddr.sin_family = AF_INET;
 
-serverAddr.sin_port = htons(7891);
+    serverAddr.sin_port = htons(7891);
 
-serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-/*Initialize size variable to be used later on*/
+    /*Initialize size variable to be used later on*/
 
-addr_size = sizeof serverAddr;
+    addr_size = sizeof serverAddr;
 
-while(1){
+    while(1){
 
-printf("Type a sentence to send to server:\n");
+        printf("Type a sentence to send to server:\n");
 
-fgets(buffer,1024,stdin);
+        fgets(buffer,1024,stdin);
 
-printf("You typed: %s",buffer);
+        printf("You typed: %s",buffer);
 
-nBytes = strlen(buffer) + 1;
+        nBytes = strlen(buffer) + 1;
 
-/*Send message to server*/
+        /*Send message to server*/
 
-sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+        sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
 
-/*Receive message from server*/
+        /*Receive message from server*/
 
-nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+        nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
 
-printf("Received from server: %s\n",buffer);
+        printf("Received from server: %s\n",buffer);
 
-}
+    }
 
-return 0;
+    return 0;
 
 }
